eaeeea2322b203565089c5d25a2d14a142adfdfd
xiu.zeng <1374040664@qq.com>
2020-04-17 11:51:37 +0800

    [zengxiu]refactor add stock-adjustment, recovery draft, change adjustment/issue/receive entry page

---
+++ src/stock-adjustment/stock-adjustment.controller.js
@@ -0,0 +1,119 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc controller
+     * @name stock-adjustment.controller:StockAdjustmentController
+     *
+     * @description
+     * Controller for making adjustment.
+     */
+    angular
+        .module('stock-adjustment')
+        .controller('StockAdjustmentController', controller);
+
+    controller.$inject = ['facility', 'programs', 'adjustmentType', '$state', 'stockmanagementUrlFactory',
+        '$http', 'user', '$q', 'loadingModalService'];
+
+    function controller(facility, programs, adjustmentType, $state, stockmanagementUrlFactory, $http, user, $q,
+                        loadingModalService) {
+        var vm = this;
+        vm.drafts = [];
+
+        /**
+         * @ngdoc property
+         * @propertyOf stock-adjustment.controller:StockAdjustmentController
+         * @name facility
+         * @type {Object}
+         *
+         * @description
+         * Holds user's home facility.
+         */
+        vm.facility = facility;
+
+        /**
+         * @ngdoc property
+         * @propertyOf stock-adjustment.controller:StockAdjustmentController
+         * @name programs
+         * @type {Array}
+         *
+         * @description
+         * Holds available programs for home facility.
+         */
+        vm.programs = [];
+
+        vm.key = function(secondaryKey) {
+            return adjustmentType.prefix + '.' + secondaryKey;
+        };
+
+        vm.proceed = function(program, draft) {
+            $state.go('openlmis.stockmanagement.' + adjustmentType.state + '.creation', {
+                programId: program.id,
+                program: program,
+                facility: facility,
+                /* SIGLUS-REFACTOR: recovery draft */
+                draft: draft,
+                draftId: draft && draft.id
+            });
+        };
+
+        vm.$onInit = function() {
+            var url = stockmanagementUrlFactory('/api/drafts');
+            loadingModalService.open();
+            var promises = _.map(programs, function(program) {
+                return $http.get(url, {
+                    params: {
+                        program: program.id,
+                        facility: facility.id,
+                        isDraft: true,
+                        userId: user.user_id,
+                        draftType: adjustmentType.state
+                    }
+                }).then(function(res) {
+                    var draft = null;
+                    if (res.data.length > 0) {
+                        draft = res.data[0];
+                    }
+                    return draft;
+                }, function() {
+                    loadingModalService.close();
+                });
+            });
+
+            $q.all(promises).then(function(drafts) {
+                loadingModalService.close();
+
+                drafts = _.filter(drafts, function(draft) {
+                    return draft !== null;
+                });
+                vm.drafts = drafts;
+
+                vm.programs = _.map(programs, function(p) {
+                    p.draft = _.find(vm.drafts, function(d) {
+                        return d.programId === p.id;
+                    });
+                    return p;
+                });
+            }, function() {
+                loadingModalService.close();
+                /* SIGLUS-REFACTOR: ends here */
+            });
+        };
+    }
+})();
---
+++ src/stock-adjustment/stock-adjustment.controller.spec.js
@@ -0,0 +1,79 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+describe('StockAdjustmentController', function() {
+
+    var vm, state, facility, programs, drafts, user;
+
+    beforeEach(function() {
+
+        module('stock-adjustment');
+
+        inject(
+            function(_messageService_, $controller, $q, ADJUSTMENT_TYPE) {
+
+                state = jasmine.createSpyObj('$state', ['go']);
+
+                programs = [{
+                    name: 'HIV',
+                    id: '1'
+                }, {
+                    name: 'TB',
+                    id: '2'
+                }];
+                facility = {
+                    id: '10134',
+                    name: 'National Warehouse',
+                    supportedPrograms: programs
+                };
+
+                /* SIGLUS-REFACTOR: recovery draft */
+                drafts = [];
+
+                user = {};
+                /* SIGLUS-REFACTOR: ends here */
+
+                vm = $controller('StockAdjustmentController', {
+                    facility: facility,
+                    programs: programs,
+                    adjustmentType: ADJUSTMENT_TYPE.ADJUSTMENT,
+                    $state: state,
+                    /* SIGLUS-REFACTOR: recovery draft */
+                    drafts: drafts,
+                    user: user
+                    /* SIGLUS-REFACTOR: ends here */
+                });
+            }
+        );
+    });
+
+    it('should go to stock adjustment draft page when proceed', function() {
+        var chooseProgram = {
+            name: 'HIV',
+            id: '1'
+        };
+
+        vm.proceed(chooseProgram);
+
+        expect(state.go).toHaveBeenCalledWith('openlmis.stockmanagement.adjustment.creation', {
+            programId: '1',
+            program: {
+                name: 'HIV',
+                id: '1'
+            },
+            facility: facility
+        });
+    });
+});
