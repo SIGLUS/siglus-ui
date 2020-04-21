88161fcc2619cbf8d3020f1d19b4d9095bd62a75
Luo Huan <huan.luo@thoughtworks.com>
2020-04-21 15:32:34 +0800

    [luohuan] #N/A refactor requisition part of create a regular requisition for the current period in the requisition-ui

---
+++ src/requisition-initiate/period.factory.js
@@ -0,0 +1,119 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc service
+     * @name requisition-initiate.periodFactory
+     *
+     * @description
+     * Responsible for parse periods and requisitions for initiate screen.
+     */
+    angular
+        .module('requisition-initiate')
+        .config(function($provide) {
+            $provide.decorator('periodFactory', decorator);
+        });
+
+    decorator.$inject = ['$delegate', 'periodService', 'messageService',
+        // '$q',
+        'REQUISITION_STATUS'];
+
+    function decorator($delegate, periodService, messageService, REQUISITION_STATUS) {
+        var periodFactory = $delegate;
+
+        periodFactory.get = get;
+
+        return periodFactory;
+
+        /**
+         * @ngdoc method
+         * @methodOf requisition-initiate.periodFactory
+         * @name get
+         *
+         * @description
+         * Retrieves periods for initiate from server.
+         *
+         * @param  {String}  programId  program UUID
+         * @param  {String}  facilityId facility UUID
+         * @param  {Boolean} emergency  if searching for emergency periods
+         * @return {Promise}            facility promise
+         */
+        function get(programId, facilityId, emergency) {
+            return periodService.getPeriodsForInitiate(programId, facilityId, emergency)
+                .then(function(response) {
+                    var periods = getPeriodGridLineItems(response, emergency);
+                    angular.forEach(periods, setStatus(emergency));
+                    return periods;
+                });
+        }
+
+        function getPeriodGridLineItems(periods, emergency) {
+            var periodGridLineItems = [];
+
+            angular.forEach(periods, function(period, id) {
+                // SIGLUS-REFACTOR: starts here
+                if (_.isEmpty(period.requisitionStatus)
+                    || period.requisitionStatus === REQUISITION_STATUS.INITIATED) {
+                // SIGLUS-REFACTOR: ends here
+                    periodGridLineItems.push(createPeriodGridItem(period, emergency, id));
+                }
+            });
+
+            return periodGridLineItems;
+        }
+
+        function createPeriodGridItem(period, emergency, id) {
+            return {
+                name: period.name,
+                startDate: period.startDate,
+                endDate: period.endDate,
+                rnrStatus: messageService.get(getRnrStatus(period, emergency, id)),
+                activeForRnr: (emergency || id === 0),
+                rnrId: (period.requisitionId) ? period.requisitionId : null
+            };
+        }
+
+        function setStatus(emergency) {
+            return function(period) {
+                if (isNotStarted(period, emergency)) {
+                    period.rnrStatus = messageService.get('requisitionInitiate.notYetStarted');
+                }
+            };
+        }
+
+        function isNotStarted(period, emergency) {
+            return emergency &&
+                (period.rnrStatus === REQUISITION_STATUS.AUTHORIZED ||
+                    period.rnrStatus === REQUISITION_STATUS.IN_APPROVAL ||
+                    period.rnrStatus === REQUISITION_STATUS.APPROVED ||
+                    period.rnrStatus === REQUISITION_STATUS.RELEASED);
+        }
+
+        function getRnrStatus(period, emergency, id) {
+            return period.requisitionStatus ?
+                period.requisitionStatus :
+                (
+                    (emergency || id === 0) ?
+                        'requisitionInitiate.notYetStarted' :
+                        'requisitionInitiate.previousPending'
+                );
+        }
+    }
+
+})();
---
+++ src/requisition-view/requisition-watcher.factory.js
@@ -0,0 +1,87 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc service
+     * @name requisition-view.RequisitionWatcher
+     *
+     * @description
+     * Provides auto-save feature to the requisition. Notifies when changes are being made to the
+     * watched requisition - this can be avoided by silencing the watcher.
+     */
+    angular
+        .module('requisition-view')
+        .factory('RequisitionWatcher', factory);
+
+    factory.$inject = ['$timeout'];
+
+    function factory($timeout) {
+
+        RequisitionWatcher.prototype.disableWatcher = disableWatcher;
+        RequisitionWatcher.prototype.enableWatcher = enableWatcher;
+
+        return RequisitionWatcher;
+
+        /**
+         * @ngdoc method
+         * @methodOf requisition-view.RequisitionWatcher
+         * @name RequisitionWatcher
+         *
+         * @description
+         * Creates requisition watcher for changes in requisition line items and comments.
+         *
+         * @param  {Scope}              scope       scope that requisition is in
+         * @param  {Object}             requisition requisition to set watcher on
+         * @return {RequisitionWatcher}             watcher object
+         */
+        function RequisitionWatcher(scope, requisition, storage) {
+            this.enabled = true;
+
+            addWatcher(scope, requisition, 'requisitionLineItems', this, storage);
+            addWatcher(scope, requisition, 'draftStatusMessage', this, storage);
+            // SIGLUS-REFACTOR: starts here
+            addWatcher(scope, requisition, 'extraData', this, storage);
+            // SIGLUS-REFACTOR: ends here
+        }
+
+        function enableWatcher() {
+            this.enabled = true;
+        }
+
+        function disableWatcher() {
+            this.enabled = false;
+        }
+
+        function addWatcher(scope, requisition, valueToWatch, watcher, storage) {
+            scope.$watch(function() {
+                return requisition[valueToWatch];
+            }, function(oldValue, newValue) {
+                if (oldValue !== newValue && watcher.enabled) {
+                    $timeout.cancel(watcher.syncTimeout);
+                    watcher.syncTimeout = $timeout(function() {
+                        requisition.$modified = true;
+                        storage.put(requisition);
+                        watcher.syncTimeout = undefined;
+                    }, 500);
+                }
+            }, true);
+        }
+    }
+
+})();
