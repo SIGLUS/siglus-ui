da7ec3ae07fccc2f1d2465faedd9fb0f227dc7fd
xiu.zeng <1374040664@qq.com>
2020-05-08 16:56:36 +0800

    [zengxiu]upgrade requisition to v3.9

--- src/requisition-view/requisition-view.controller.spec.js
+++ src/requisition-view/requisition-view.controller.spec.js
@@ -344,7 +344,7 @@ describe('RequisitionViewController', function() {
             expect(this.loadingModalService.open).toHaveBeenCalled();
         });
 
-        it('should reload state without requisition param', function() {
+        it('should reload state', function() {
             this.requisition.$save.andReturn(this.$q.resolve(true));
             this.loadingModalService.open.andReturn(this.$q.resolve());
 
@@ -352,12 +352,7 @@ describe('RequisitionViewController', function() {
             this.vm.syncRnr();
             this.$rootScope.$apply();
 
-            expect(this.$state.go).toHaveBeenCalledWith(this.$state.current, {
-                rnr: this.requisition.id,
-                requisition: undefined
-            }, {
-                reload: true
-            });
+            expect(this.$state.reload).toHaveBeenCalled();
         });
 
     });
@@ -729,18 +724,13 @@ describe('RequisitionViewController', function() {
             expect(this.loadingModalService.open.callCount).toEqual(1);
         });
 
-        it('should reload state with proper parameters', function() {
+        it('should reload state', function() {
             this.requisition.$save.andReturn(this.$q.resolve(true));
 
             this.vm.syncRnrAndPrint();
             this.$rootScope.$apply();
 
-            expect(this.$state.go).toHaveBeenCalledWith(this.$state.current, {
-                rnr: this.vm.requisition.id,
-                requisition: undefined
-            }, {
-                reload: true
-            });
+            expect(this.$state.reload).toHaveBeenCalled();
         });
     });
 
