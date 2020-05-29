c7b341e71a610fd9d3b41387698f4d33c70f09b5
wang xiu <xiu.wang@thoughtworks.com>
2020-05-29 13:41:48 +0800

    [xiuwang]#173: validate template if user  route to template tab related page.

---
+++ src/admin-template-configure/admin-template-configure.controller.js
@@ -0,0 +1,94 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc controller
+     * @name admin-template-configure.controller:AdminTemplateConfigureController
+     *
+     * @description
+     * Controller for managing template configuration screen.
+     */
+    angular
+        .module('admin-template-configure')
+        .controller('AdminTemplateConfigureController', AdminTemplateConfigureController);
+
+    AdminTemplateConfigureController.$inject = [
+        'template', 'program',
+        // #173: product sections for template configuration
+        'configureStateRouterService'
+        // #173: ends here
+    ];
+
+    function AdminTemplateConfigureController(template, program, configureStateRouterService) {
+
+        var vm = this;
+
+        vm.$onInit = onInit;
+
+        /**
+         * @ngdoc property
+         * @propertyOf admin-template-configure.controller:AdminTemplateConfigureController
+         * @name template
+         * @type {Object}
+         *
+         * @description
+         * Holds template.
+         */
+        vm.template = template;
+
+        /**
+         * @ngdoc property
+         * @propertyOf admin-template-configure.controller:AdminTemplateConfigureController
+         * @name program
+         * @type {Object}
+         *
+         * @description
+         * Holds program.
+         */
+        vm.program = program;
+
+        /**
+         * @ngdoc property
+         * @propertyOf admin-template-configure.controller:AdminTemplateConfigureController
+         * @type {string}
+         * @name originalTemplateName
+         *
+         * @description
+         * The original name of the template.
+         */
+        vm.originalTemplateName = undefined;
+
+        /**
+         * @ngdoc method
+         * @methodOf admin-template-configure.controller:AdminTemplateConfigureController
+         * @name onInit
+         *
+         * @description
+         * Initialization method of the AdminTemplateConfigureController.
+         */
+        function onInit() {
+            vm.template = template;
+            vm.program = program;
+            vm.originalTemplateName = template.name;
+            // #173: product sections for template configuration
+            configureStateRouterService.initialize(template);
+            // #173: ends here
+        }
+    }
+})();
---
+++ src/admin-template-configure/configure-state-router.service.js
@@ -0,0 +1,47 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+    'use strict';
+
+    /**
+     * @ngdoc service
+     * @name admin-template-configure.configureStateRouterService
+     *
+     * @description
+     * Check if state being transitioned is valid.
+     */
+    angular.module('admin-template-configure')
+        .service('configureStateRouterService', service);
+
+    service.$inject = [
+        '$rootScope', 'loadingModalService', 'notificationService'
+    ];
+
+    function service($rootScope, loadingModalService, notificationService) {
+        this.initialize = initialize;
+        function initialize(template) {
+            $rootScope.$on('$stateChangeStart', function(event, toState) {
+                if (toState.name.indexOf('openlmis.administration.requisitionTemplates.configure') > -1
+                && !template.isValid()) {
+                    event.preventDefault();
+                    loadingModalService.close();
+                    notificationService.error('adminProgramTemplate.template.invalid');
+                }
+            });
+        }
+    }
+
+})();
