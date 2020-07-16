12e5a458ab015e73fe165a4e6f3d0d38db0233af
Luo Huan <huan.luo@thoughtworks.com>
2020-07-16 14:24:32 +0800

    [huanluo] #375: add test consumption form of siglus test consumption component

--- src/requisition-view-section/siglus-test-consumption.controller.spec.js
+++ src/requisition-view-section/siglus-test-consumption.controller.spec.js
@@ -15,7 +15,7 @@
 
 describe('SiglusTestConsumptionController', function() {
 
-    var vm, $controller, program, ProgramDataBuilder;
+    var vm, $controller, program, ProgramDataBuilder, sections, lineItems, canEdit;
 
     beforeEach(function() {
         module('requisition-view-section');
@@ -25,10 +25,108 @@ describe('SiglusTestConsumptionController', function() {
             ProgramDataBuilder = $injector.get('ProgramDataBuilder');
         });
 
-        program = new ProgramDataBuilder().withId('program-1')
-            .build();
+        program = new ProgramDataBuilder().build();
+        sections = [{
+            name: 'project',
+            label: 'Test Project',
+            displayOrder: 0,
+            columns: [{
+                name: 'hivDetermine',
+                label: 'HIV Determine',
+                indicator: 'TP',
+                displayOrder: 0,
+                isDisplayed: true,
+                option: null,
+                definition: 'record the test data for HIV Determine',
+                tag: null,
+                columnDefinition: {},
+                source: null
+            }]
+        }, {
+            name: 'outcome',
+            label: 'Test Outcome',
+            displayOrder: 1,
+            columns: [{
+                name: 'consumo',
+                label: 'Consumo',
+                indicator: 'TO',
+                displayOrder: 0,
+                isDisplayed: true,
+                option: null,
+                definition: 'record the consumo quantity for each test project',
+                tag: null,
+                columnDefinition: {},
+                source: 'USER_INPUT'
+            }, {
+                name: 'new',
+                label: 'new',
+                indicator: 'n',
+                displayOrder: 2,
+                isDisplayed: true,
+                option: null,
+                definition: 'record the new column',
+                tag: null,
+                columnDefinition: {},
+                source: 'USER_INPUT'
+            }]
+        }, {
+            name: 'service',
+            label: 'Services',
+            displayOrder: 2,
+            columns: [ {
+                name: 'HF',
+                label: 'HF',
+                indicator: 'SV',
+                displayOrder: 0,
+                isDisplayed: false,
+                option: null,
+                definition: 'record the test outcome for my facility',
+                tag: null,
+                columnDefinition: {},
+                source: 'USER_INPUT'
+            }, {
+                name: 'total',
+                label: 'Total',
+                indicator: 'SV',
+                displayOrder: 1,
+                isDisplayed: true,
+                option: null,
+                definition: 'record the total number of each column',
+                tag: null,
+                columnDefinition: {},
+                source: 'USER_INPUT'
+            }]
+        }];
+        lineItems = [
+            {
+                service: 'total',
+                projects: {
+                    hivDetermine: {
+                        project: 'hivDetermine',
+                        outcomes: {
+                            consumo: {
+                                outcome: 'consumo',
+                                value: null
+                            },
+                            positive: {
+                                outcome: 'positive',
+                                value: null
+                            },
+                            unjustified: {
+                                outcome: 'unjustified',
+                                value: null
+                            }
+                        }
+                    }
+                }
+            }
+        ];
+        canEdit = true;
         vm = $controller('SiglusTestConsumptionController');
         vm.program = program;
+        vm.sections = sections;
+        vm.lineItems = lineItems;
+        vm.canEdit = canEdit;
         vm.$onInit();
     });
 
@@ -37,6 +135,84 @@ describe('SiglusTestConsumptionController', function() {
         it('should bind program to vm', function() {
 
             expect(vm.program).toEqual(program);
+        });
+
+        it('should bind sections to vm', function() {
+
+            expect(vm.sections).toEqual(sections);
+        });
+
+        it('should bind canEdit to vm', function() {
+
+            expect(vm.canEdit).toEqual(canEdit);
+        });
+
+        it('should set correct testProjectColspan and bind to vm', function() {
+
+            expect(vm.testProjectColspan).toEqual(2);
+        });
+
+        it('should set correct programColspan and bind to vm', function() {
+
+            expect(vm.programColspan).toEqual(3);
+        });
+
+        it('should enhance lineItems and bind to vm', function() {
+
+            expect(vm.lineItems).toEqual([
+                {
+                    service: 'total',
+                    name: 'total',
+                    label: 'Total',
+                    indicator: 'SV',
+                    displayOrder: 1,
+                    isDisplayed: true,
+                    option: null,
+                    definition: 'record the total number of each column',
+                    tag: null,
+                    columnDefinition: {},
+                    source: 'USER_INPUT',
+                    projects: {
+                        hivDetermine: {
+                            project: 'hivDetermine',
+                            name: 'hivDetermine',
+                            label: 'HIV Determine',
+                            indicator: 'TP',
+                            displayOrder: 0,
+                            isDisplayed: true,
+                            option: null,
+                            definition: 'record the test data for HIV Determine',
+                            tag: null,
+                            columnDefinition: {},
+                            source: null,
+                            outcomes: {
+                                consumo: {
+                                    outcome: 'consumo',
+                                    value: null,
+                                    name: 'consumo',
+                                    label: 'Consumo',
+                                    indicator: 'TO',
+                                    displayOrder: 0,
+                                    isDisplayed: true,
+                                    option: null,
+                                    definition: 'record the consumo quantity for each test project',
+                                    tag: null,
+                                    columnDefinition: {},
+                                    source: 'USER_INPUT'
+                                },
+                                positive: {
+                                    outcome: 'positive',
+                                    value: null
+                                },
+                                unjustified: {
+                                    outcome: 'unjustified',
+                                    value: null
+                                }
+                            }
+                        }
+                    }
+                }
+            ]);
         });
     });
 });
