e7de556be4883d2c3928ef13e462581fef596bab
Luo Huan <huan.luo@thoughtworks.com>
2020-09-08 14:44:27 +0800

    [huanluo] #517: remove unnecessary changes

--- src/requisition/requisition.js
+++ src/requisition/requisition.js
@@ -291,7 +291,7 @@
                 id = this.id;
             return handlePromise(resource.save({
                 id: this.id
-            }, angular.copy(this)).$promise, function(saved) {
+            }, this).$promise, function(saved) {
                 saveToStorage(saved, availableOffline);
             }, function(saved) {
                 if (saved.status === 409 || saved.status === 403) {
