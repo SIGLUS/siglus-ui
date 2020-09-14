9f48e8727b6f3f9373cf91bfa391682c3c56d06a
wang xiu <xiu.wang@thoughtworks.com>
2020-09-14 15:05:45 +0800

    [xiuwang]#545: add notification new UI, and add todo and update notification type.

---
+++ src/siglus-notification/siglus-notification-item.js
@@ -0,0 +1,168 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc service
+     * @name siglus-notification.Notification
+     *
+     * @description
+     * Responsible for adding required methods for line items.
+     */
+    angular
+        .module('siglus-notification')
+        .factory('NotificationItem', notificationItem);
+
+    notificationItem.$inject = ['openlmisDateFilter', 'REQUISITION_STATUS', 'ORDER_STATUS', 'NOTIFICATION_TYPE',
+        '$state'];
+
+    function notificationItem(openlmisDateFilter, REQUISITION_STATUS, ORDER_STATUS, NOTIFICATION_TYPE, $state) {
+
+        var notificationMsgConfig = {
+            UPDATE: {
+                APPROVED: {
+                    class: 'requisition',
+                    label: 'notification.requisition',
+                    content: 'notification.update.requisition.approved.message',
+                    date: 'notification.update.requisition.approved.date'
+                },
+                SHIPPED: {
+                    class: 'order',
+                    label: 'notification.order',
+                    content: 'notification.update.order.shipped.message',
+                    date: 'notification.update.order.shipped.date'
+                },
+                RECEIVED: {
+                    class: 'pod',
+                    label: 'notification.pod',
+                    content: 'notification.update.pod.received.message',
+                    date: 'notification.update.pod.received.date'
+                }
+            },
+            TODO: {
+                REJECTED: {
+                    class: 'requisition',
+                    label: 'notification.requisition',
+                    content: 'notification.todo.requisition.rejected.message',
+                    date: 'notification.todo.requisition.rejected.date'
+                },
+                SUBMITTED: {
+                    class: 'requisition',
+                    label: 'notification.requisition',
+                    content: 'notification.todo.requisition.submitted.message',
+                    date: 'notification.todo.requisition.submitted.date'
+                },
+                AUTHORIZED: {
+                    class: 'requisition',
+                    label: 'notification.requisition',
+                    content: 'notification.todo.requisition.toApprove.message',
+                    date: 'notification.todo.requisition.toApprove.date'
+                },
+                IN_APPROVAL: {
+                    class: 'requisition',
+                    label: 'notification.requisition',
+                    content: 'notification.todo.requisition.toApprove.message',
+                    date: 'notification.todo.requisition.toApprove.date'
+                },
+                APPROVED: {
+                    class: 'requisition',
+                    label: 'notification.requisition',
+                    content: 'notification.update.requisition.approved.message',
+                    date: 'notification.update.requisition.approved.date'
+                },
+                ORDERED: {
+                    class: 'order',
+                    label: 'notification.order',
+                    content: 'notification.todo.order.ordered.message',
+                    date: 'notification.todo.order.ordered.date'
+                },
+                SHIPPED: {
+                    class: 'pod',
+                    label: 'notification.pod',
+                    content: 'notification.todo.order.shipped.message',
+                    date: 'notification.todo.order.shipped.date'
+                }
+            }
+        };
+
+        NotificationItem.prototype.navigate = navigate;
+
+        return NotificationItem;
+
+        function NotificationItem(notificationItem) {
+            angular.copy(notificationItem, this);
+
+            this.createdDate = openlmisDateFilter(new Date(notificationItem.createdDate));
+            this.requisitionSubmittedDate = notificationItem.requisitionSubmittedDate ?
+                openlmisDateFilter(new Date(notificationItem.requisitionSubmittedDate)) : null;
+            this.config = notificationMsgConfig[notificationItem.type][notificationItem.status];
+        }
+
+        function navigate() {
+            if (this.type === NOTIFICATION_TYPE.TODO) {
+                todoNavigate(this);
+            }
+            if (this.type === NOTIFICATION_TYPE.UPDATE) {
+                updateNavigate(this);
+            }
+        }
+
+        function todoNavigate(notificationItem) {
+            if (notificationItem.status === REQUISITION_STATUS.APPROVED) {
+                goToConvertOrder();
+            } else if (notificationItem.status === ORDER_STATUS.ORDERED) {
+                goToShipment(notificationItem);
+            } else if (notificationItem.status === ORDER_STATUS.SHIPPED) {
+                goToPOD(notificationItem);
+            } else {
+                goToRequisition(notificationItem);
+            }
+        }
+
+        function updateNavigate(notificationItem) {
+            if (notificationItem.status === REQUISITION_STATUS.APPROVED) {
+                goToRequisition(notificationItem);
+            } else {
+                goToPOD(notificationItem);
+            }
+        }
+
+        function goToRequisition(notificationItem) {
+            $state.go('openlmis.requisitions.requisition.fullSupply', {
+                rnr: notificationItem.referenceId
+            });
+        }
+
+        function goToConvertOrder() {
+            $state.go('openlmis.requisitions.convertToOrder');
+        }
+
+        function goToShipment(notificationItem) {
+            $state.go('openlmis.orders.shipmentView', {
+                id: notificationItem.referenceId
+            });
+        }
+
+        function goToPOD(notificationItem) {
+            $state.go('openlmis.orders.podManage.podView', {
+                podId: notificationItem.referenceId
+            });
+        }
+    }
+
+})();
---
+++ src/siglus-notification/siglus-notification.spec.js
@@ -0,0 +1,118 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+describe('Notification', function() {
+
+    beforeEach(function() {
+        module('siglus-notification');
+
+        inject(function($injector) {
+            this.openlmisDateFilter = $injector.get('openlmisDateFilter');
+            this.REQUISITION_STATUS = $injector.get('REQUISITION_STATUS');
+            this.ORDER_STATUS = $injector.get('ORDER_STATUS');
+            this.NOTIFICATION_TYPE = $injector.get('NOTIFICATION_TYPE');
+            this.$state = $injector.get('$state');
+            this.Notification = $injector.get('Notification');
+        });
+
+        this.notification = new this.Notification({
+            createdDate: '2020-09-11T07:40:52.753Z',
+            emergency: false,
+            id: 'fa907bec-65d3-4892-9f31-06708118e5c4',
+            processingPeriod: {
+                endDate: '2020-03-31',
+                id: 'a2d7768e-4218-4c7e-8afa-7e3844f5300b',
+                name: '202003-multi',
+                startDate: '2020-03-01'
+            },
+            referenceId: 'f433700a-1182-41a9-8dda-30cb1c1681c2',
+            requestingFacility: null,
+            requisitionSubmittedDate: null,
+            status: 'SHIPPED',
+            type: 'TODO'
+        });
+    });
+
+    it('Should format createdDate', function() {
+        expect(this.notification.createdDate).toBe('11/09/2020');
+    });
+
+    it('Should add notification config', function() {
+        expect(this.notification.config).toEqual({
+            class: 'pod',
+            label: 'notification.pod',
+            content: 'notification.todo.order.shipped.message',
+            date: 'notification.todo.order.shipped.date'
+        });
+    });
+
+    describe('navigate', function() {
+        beforeEach(function() {
+            spyOn(this.$state, 'go').andReturn();
+        });
+
+        it('Should route to convert to order when notification type is todo and status is approved', function() {
+            this.notification.status = 'APPROVED';
+            this.notification.navigate();
+
+            expect(this.$state.go).toHaveBeenCalledWith('openlmis.requisitions.convertToOrder');
+        });
+
+        it('Should route to shipment when notification type is todo and status is ORDERED', function() {
+            this.notification.status = 'ORDERED';
+            this.notification.navigate();
+
+            expect(this.$state.go).toHaveBeenCalledWith('openlmis.orders.shipmentView', {
+                id: 'f433700a-1182-41a9-8dda-30cb1c1681c2'
+            });
+        });
+
+        it('Should route to POD when notification type is todo and status is SHIPPED', function() {
+            this.notification.navigate();
+
+            expect(this.$state.go).toHaveBeenCalledWith('openlmis.orders.podManage.podView', {
+                podId: 'f433700a-1182-41a9-8dda-30cb1c1681c2'
+            });
+        });
+
+        it('Should route to requisition when notification type is todo and status is SUBMITTED', function() {
+            this.notification.status = 'SUBMITTED';
+            this.notification.navigate();
+
+            expect(this.$state.go).toHaveBeenCalledWith('openlmis.requisitions.requisition.fullSupply', {
+                rnr: 'f433700a-1182-41a9-8dda-30cb1c1681c2'
+            });
+        });
+
+        it('Should route to POD when notification type is update and status is SHIPPED', function() {
+            this.notification.navigate();
+
+            expect(this.$state.go).toHaveBeenCalledWith('openlmis.orders.podManage.podView', {
+                podId: 'f433700a-1182-41a9-8dda-30cb1c1681c2'
+            });
+        });
+
+        it('Should route to requisition when notification type is update and status is APPROVED', function() {
+            this.notification.status = 'APPROVED';
+            this.notification.type = 'UPDATE';
+            this.notification.navigate();
+
+            expect(this.$state.go).toHaveBeenCalledWith('openlmis.requisitions.requisition.fullSupply', {
+                rnr: 'f433700a-1182-41a9-8dda-30cb1c1681c2'
+            });
+        });
+
+    });
+});
