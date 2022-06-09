dc0837499f3639bb1b5b6aed45a7d66a4cf9099b
ke.dang <ke.dang@thoughtworks.com>
2022-06-09 14:34:42 +0800

    [dangke] #9 add router for Expired Products Report

--- src/siglus-analytics-report-metabase/siglus-analytics-report-metabase-constant.js
+++ src/siglus-analytics-report-metabase/siglus-analytics-report-metabase-constant.js
@@ -30,7 +30,8 @@
         return {
             SYSTEM_VERSION_REPORT: 'system_version_report',
             EXPIRING_PRODUCTS_REPORT: 'expiring_products_report',
-            SYSTEM_UPDATE_REPORT: 'system_update_report'
+            SYSTEM_UPDATE_REPORT: 'system_update_report',
+            EXPIRED_PRODUCTS_REPORT: 'expired_products_report'
         };
     }
 
