f145e0eac18067fe8603940dd736626048b4ecbe
tw-liuzhangren <zhangren.liu@thoughtworks.com>
2022-06-09 17:56:29 +0800

    rapid test card start~

---
+++ src/siglus-analytics-report-customize-rapid/siglus-analytics-report-customize-rapid.routes.js
@@ -0,0 +1,49 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+    'use strict';
+
+    angular
+        .module('siglus-analytics-report-customize-rapid')
+        .config(routes);
+
+    routes.$inject = ['$stateProvider'];
+
+    function routes($stateProvider) {
+
+        $stateProvider.state('openlmis.analyticsReport.rapid', {
+            url: '/rapid',
+            showInNavigation: false,
+            label: 'rapid.title',
+            priority: 9,
+            views: {
+                '@openlmis': {
+                    controller: 'siglusAnalyticsReportCustomizeRapidController',
+                    controllerAs: 'vm',
+                    // eslint-disable-next-line max-len
+                    templateUrl: 'siglus-analytics-report-customize-rapid/siglus-analytics-report-customize-rapid.html'
+                }
+            }
+            // resolve: {
+            //     facility: function(facilityService, requisition) {
+            //         return facilityService.get(requisition.facility.id);
+            //     }
+            // }
+        });
+    }
+
+})();
