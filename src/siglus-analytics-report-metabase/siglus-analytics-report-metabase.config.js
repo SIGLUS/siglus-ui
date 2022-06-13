1452f1852b0cd01b3909bd803f0dd24b2f1f42a3
jihao-huang <jihao.huang@thoughtworks.com>
2022-06-13 17:13:12 +0800

    [jihao] #NA add resourceUrlWhiteList for iframe to load metabase

--- src/siglus-analytics-report-metabase/siglus-analytics-report-metabase.config.js
+++ src/siglus-analytics-report-metabase/siglus-analytics-report-metabase.config.js
@@ -28,7 +28,9 @@
             // Allow same origin resource loads.
             'self',
             // Allow loading from our assets domain.  Notice the difference between * and **.
-            'https://qa-metabase.siglus.us/**'
+            'https://qa-metabase.siglus.us/**',
+            'https://dev-metabase.siglus.us/**',
+            'https://uat-metabase.siglus.us/**'
         ]);
     }
 
