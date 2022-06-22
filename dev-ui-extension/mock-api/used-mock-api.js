2b43fbb20136f9a42842eb6ca406591f42a34bf4
tw-liuzhangren <zhangren.liu@thoughtworks.com>
2022-06-22 10:01:25 +0800

    temp

--- dev-ui-extension/mock-api/used-mock-api.js
+++ dev-ui-extension/mock-api/used-mock-api.js
@@ -12,6 +12,41 @@ const usedMockAPIs = {
     },
     '*/api/demo-request-handler*': function(req, res, next) {
         res.end(JSON.stringify({'data': []}));
+    },
+    '*/api/get-conflict-draft*': function(req, res, next) {
+        const { facilityId, programId } = req
+        let result;
+        if (facilityId == 100 && programId == 110) {
+            result = [];
+        } else {
+            result = [
+                {
+                    orderable: {
+                        productCode: '001',
+                        fullProductName: 'Imunoglobulina humana anti-rabica inj; 1000UI/5mL; Inj',
+                    },
+                    currentStock: 0,
+                    conflictWith: 'draft1'
+                },
+                {
+                    orderable: {
+                        productCode: '002',
+                        fullProductName: 'Imunoglobulina humana anti-rabica inj; 1000UI/5mL; Inj',
+                    },
+                    currentStock: 0,
+                    conflictWith: 'draft2'
+                },
+                {
+                    orderable: {
+                        productCode: '003',
+                        fullProductName: 'Imunoglobulina humana anti-rabica inj; 1000UI/5mL; Inj',
+                    },
+                    currentStock: 0,
+                    conflictWith: 'draft3'
+                }
+            ]
+        }
+        res.end(JSON.stringify({'data': result}));
     }
 };
 
