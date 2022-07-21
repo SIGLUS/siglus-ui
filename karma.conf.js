84929c78197b64eca7ed1e00b2b3424add47cc0d
wangjie <darrewang@thoughtworks.com>
2022-07-21 11:40:57 +0800

    [wang jie] #110 fixed sonar error

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
             '.tmp/javascript/src/**/*.routes.js',
             '.tmp/javascript/src/**/*.js',
             '.tmp/javascript/tests/**/*builder.spec.js',
-            // 'src/**/*.js',
+            'src/**/*.js',
             'src/**/*.spec.js'
         ],
 
