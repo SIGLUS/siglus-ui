d1391031731da8dd9c566ec0c94f33e1716fbf4e
wangjie <darrewang@thoughtworks.com>
2022-08-05 11:14:34 +0800

    [wang jie] #239 change test coverage threshold

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-coverage = $(`grep -o -P '(?<=<span class="strong">).*(?=% </span>)' 'build/test/coverage/HeadlessChrome\\ 74.0.3723\\ \\(Linux\\ 0.0.0\\)/lcov-report/index.html' | head -1`)
