b5d336c78f1ee608e5c4d62cdf35ea7a83b60705
jihao-huang <jihao.huang@thoughtworks.com>
2022-09-29 11:29:56 +0800

    [jihao] #442 add filter when add product for issue3 and add blank for expired

--- src/requisition-view/full-supply.routes.js
+++ src/requisition-view/full-supply.routes.js
@@ -59,10 +59,10 @@
                             // eslint-disable-next-line max-len
                             // SIGLUS-REFACTOR: starts here :after add product or change page number, the product order will restore by this filter
                             //, just order by the display the order will not change when page number change
-                            // '$program.orderableCategoryDisplayOrder',
-                            // '$program.orderableCategoryDisplayName',
-                            '$program.displayOrder'
-                            // 'orderable.fullProductName'
+                            '$program.orderableCategoryDisplayOrder',
+                            '$program.orderableCategoryDisplayName',
+                            '$program.displayOrder',
+                            'orderable.fullProductName'
                             // SIGLUS-REFACTOR: ends here
                         ]);
                     },
