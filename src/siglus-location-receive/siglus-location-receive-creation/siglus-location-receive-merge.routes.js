a736bd4f657fb5e9678f9b3fa5588870de1980db
wangjie <darrewang@thoughtworks.com>
2022-10-19 12:22:39 +0800

    [wangjie] #393 feat: add shipment print logic

--- src/siglus-location-receive/siglus-location-receive-creation/siglus-location-receive-merge.routes.js
+++ src/siglus-location-receive/siglus-location-receive-creation/siglus-location-receive-merge.routes.js
@@ -82,7 +82,7 @@
                     if ($stateParams.productList) {
                         return $stateParams.productList;
                     }
-                    return siglusLocationCommonApiService.getProductList(false, $stateParams.draftId);
+                    return siglusLocationCommonApiService.getAllProductList($stateParams.draftId);
                 },
                 mergedItems: function($stateParams, siglusStockIssueLocationService, alertService) {
                     if ($stateParams.mergedItems) {
